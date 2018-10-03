a854dc84947be8f50e9cf3a4cd2637fe460eaef8
José Manuel Gómez Pérez <jmgomez@iberia.expertsystem.local>
2018-10-03 16:12:19 +0200

    Updated qualitative analysis data load

--- scripts/crossmodal_analysis/data_loading.py
+++ scripts/crossmodal_analysis/data_loading.py
@@ -103,7 +103,7 @@ def file_download():
     r = requests.get(url, allow_redirects=True)
     open(captions_5class_cross_weights, 'wb').write(r.content)
 
-    print("EXPERIMENT #4 DATA LOADED") 
+    
 
     figures_2clusters_cross = "figures_2clusters_cross.h5"
     url = "https://zenodo.org/record/1442704/files/figures_2clusters_cross.h5"
@@ -125,6 +125,39 @@ def file_download():
     r = requests.get(url, allow_redirects=True)
     open(figures_5class_cross_weights, 'wb').write(r.content)
 
+	print("EXPERIMENT #4 DATA LOADED") 
+	
+	quality2clusters = "quality2clusters.h5"
+    url = "https://zenodo.org/record/1442704/files/quality2clusters.h5"
+    r = requests.get(url, allow_redirects=True)
+    open(quality2clusters, 'wb').write(r.content)
+	
+	quality5class = "quality5class.h5"
+    url = "https://zenodo.org/record/1442704/files/quality5class.h5"
+    r = requests.get(url, allow_redirects=True)
+    open(quality5class, 'wb').write(r.content)
+	
+	qualityMix2clusters = "qualityMix2clusters.h5"
+    url = "https://zenodo.org/record/1442704/files/qualityMix2clusters.h5"
+    r = requests.get(url, allow_redirects=True)
+    open(qualityMix2clusters, 'wb').write(r.content)
+	
+	qualityMix5class = "qualityMix5class.h5"
+    url = "https://zenodo.org/record/1442704/files/qualityMix5class.h5"
+    r = requests.get(url, allow_redirects=True)
+    open(qualityMix5class, 'wb').write(r.content)
+	
+	qualityUni2clusters = "qualityUni2clusters.h5"
+    url = "https://zenodo.org/record/1442704/files/qualityUni2clusters.h5"
+    r = requests.get(url, allow_redirects=True)
+    open(qualityUni2clusters, 'wb').write(r.content)
+	
+	qualityUni5class = "qualityUni5class.h5"
+    url = "https://zenodo.org/record/1442704/files/qualityUni5class.h5"
+    r = requests.get(url, allow_redirects=True)
+    open(qualityUni5class, 'wb').write(r.content)
+		
+	
     print("EXPERIMENT #5 DATA LOADED")
 
 def gen_text (h5path, indices,batchSize, shuffle): 

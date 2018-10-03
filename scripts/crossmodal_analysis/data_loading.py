79c5a407e9f7718811e554bfe031ae485d0a4ecf
José Manuel Gómez Pérez <jmgomez@iberia.expertsystem.local>
2018-10-03 16:26:42 +0200

    Updated qualitative analysis data load

--- scripts/crossmodal_analysis/data_loading.py
+++ scripts/crossmodal_analysis/data_loading.py
@@ -102,8 +102,7 @@ def file_download():
     url = "https://zenodo.org/record/1442704/files/captions_5class_cross_weights.h5"
     r = requests.get(url, allow_redirects=True)
     open(captions_5class_cross_weights, 'wb').write(r.content)
-
-    
+	
 
     figures_2clusters_cross = "figures_2clusters_cross.h5"
     url = "https://zenodo.org/record/1442704/files/figures_2clusters_cross.h5"
@@ -124,35 +123,35 @@ def file_download():
     url = "https://zenodo.org/record/1442704/files/figures_5class_cross_weights.h5"
     r = requests.get(url, allow_redirects=True)
     open(figures_5class_cross_weights, 'wb').write(r.content)
-
-	print("EXPERIMENT #4 DATA LOADED") 
+    
+    print("EXPERIMENT #4 DATA LOADED")
 	
-	quality2clusters = "quality2clusters.h5"
+    quality2clusters = "quality2clusters.h5"
     url = "https://zenodo.org/record/1442704/files/quality2clusters.h5"
     r = requests.get(url, allow_redirects=True)
     open(quality2clusters, 'wb').write(r.content)
 	
-	quality5class = "quality5class.h5"
+    quality5class = "quality5class.h5"
     url = "https://zenodo.org/record/1442704/files/quality5class.h5"
     r = requests.get(url, allow_redirects=True)
     open(quality5class, 'wb').write(r.content)
-	
-	qualityMix2clusters = "qualityMix2clusters.h5"
+    
+    qualityMix2clusters = "qualityMix2clusters.h5"
     url = "https://zenodo.org/record/1442704/files/qualityMix2clusters.h5"
     r = requests.get(url, allow_redirects=True)
     open(qualityMix2clusters, 'wb').write(r.content)
 	
-	qualityMix5class = "qualityMix5class.h5"
+    qualityMix5class = "qualityMix5class.h5"
     url = "https://zenodo.org/record/1442704/files/qualityMix5class.h5"
     r = requests.get(url, allow_redirects=True)
     open(qualityMix5class, 'wb').write(r.content)
 	
-	qualityUni2clusters = "qualityUni2clusters.h5"
+    qualityUni2clusters = "qualityUni2clusters.h5"
     url = "https://zenodo.org/record/1442704/files/qualityUni2clusters.h5"
     r = requests.get(url, allow_redirects=True)
     open(qualityUni2clusters, 'wb').write(r.content)
 	
-	qualityUni5class = "qualityUni5class.h5"
+    qualityUni5class = "qualityUni5class.h5"
     url = "https://zenodo.org/record/1442704/files/qualityUni5class.h5"
     r = requests.get(url, allow_redirects=True)
     open(qualityUni5class, 'wb').write(r.content)

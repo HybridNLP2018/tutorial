f989b79134a6489ed9d7fb669d933c91b9004a3f
José Manuel Gómez Pérez <jmgomez@iberia.expertsystem.local>
2018-10-04 10:16:30 +0200

    Upload shortened version of scimgmnt nb

--- scripts/crossmodal_analysis/data_loading.py
+++ scripts/crossmodal_analysis/data_loading.py
@@ -5,15 +5,15 @@ import numpy as np
 def file_download():
     print("LOADING DATASETS AND MODEL WEIGHTS")
 
-    title_abstract_2clusters = "title_abstract_2clusters.h5"
-    url = "https://zenodo.org/record/1442704/files/title_abstract_2clusters.h5"
-    r = requests.get(url, allow_redirects=True)
-    open(title_abstract_2clusters, 'wb').write(r.content)
+#    title_abstract_2clusters = "title_abstract_2clusters.h5"
+#    url = "https://zenodo.org/record/1442704/files/title_abstract_2clusters.h5"
+#    r = requests.get(url, allow_redirects=True)
+#    open(title_abstract_2clusters, 'wb').write(r.content)
 
-    title_abstract_2clusters_weights = "title_abstract_2clusters_weights.h5"
-    url = "https://zenodo.org/record/1442704/files/title_abstract_2clusters_weights.h5"
-    r = requests.get(url, allow_redirects=True)
-    open(title_abstract_2clusters_weights, 'wb').write(r.content)
+#   title_abstract_2clusters_weights = "title_abstract_2clusters_weights.h5"
+#    url = "https://zenodo.org/record/1442704/files/title_abstract_2clusters_weights.h5"
+#    r = requests.get(url, allow_redirects=True)
+#    open(title_abstract_2clusters_weights, 'wb').write(r.content)
 
     title_abstract_5class = "title_abstract_5class.h5"
     url = "https://zenodo.org/record/1442704/files/title_abstract_5class.h5"
@@ -27,15 +27,15 @@ def file_download():
 
     print("BASELINE DATA AND WEIGHTS LOADED") 
 
-    captions_2clusters = "captions_2clusters.h5"
-    url = "https://zenodo.org/record/1442704/files/captions_2clusters.h5"
-    r = requests.get(url, allow_redirects=True)
-    open(captions_2clusters, 'wb').write(r.content)
+#   captions_2clusters = "captions_2clusters.h5"
+#    url = "https://zenodo.org/record/1442704/files/captions_2clusters.h5"
+#    r = requests.get(url, allow_redirects=True)
+#    open(captions_2clusters, 'wb').write(r.content)
 
-    captions_2clusters_weights = "captions_2clusters_weights.h5"
-    url = "https://zenodo.org/record/1442704/files/captions_2clusters_weights.h5"
-    r = requests.get(url, allow_redirects=True)
-    open(captions_2clusters_weights, 'wb').write(r.content)
+#    captions_2clusters_weights = "captions_2clusters_weights.h5"
+#    url = "https://zenodo.org/record/1442704/files/captions_2clusters_weights.h5"
+#    r = requests.get(url, allow_redirects=True)
+#    open(captions_2clusters_weights, 'wb').write(r.content)
 
     captions_5class = "captions_5class.h5"
     url = "https://zenodo.org/record/1442704/files/captions_5class.h5"
@@ -49,15 +49,15 @@ def file_download():
 
     print("EXPERIMENT #1 LOADED") 
 
-    figures_2clusters = "figures_2clusters.h5"
-    url = "https://zenodo.org/record/1442704/files/figures_2clusters.h5"
-    r = requests.get(url, allow_redirects=True)
-    open(figures_2clusters, 'wb').write(r.content)
+#    figures_2clusters = "figures_2clusters.h5"
+#    url = "https://zenodo.org/record/1442704/files/figures_2clusters.h5"
+#    r = requests.get(url, allow_redirects=True)
+#    open(figures_2clusters, 'wb').write(r.content)
 
-    figures_2clusters_weights = "figures_2clusters_weights.h5"
-    url = "https://zenodo.org/record/1442704/files/figures_2clusters_weights.h5"
-    r = requests.get(url, allow_redirects=True)
-    open(figures_2clusters_weights, 'wb').write(r.content)
+#    figures_2clusters_weights = "figures_2clusters_weights.h5"
+#    url = "https://zenodo.org/record/1442704/files/figures_2clusters_weights.h5"
+#    r = requests.get(url, allow_redirects=True)
+#    open(figures_2clusters_weights, 'wb').write(r.content)
 
     figures_5class = "figures_5class.h5"
     url = "https://zenodo.org/record/1442704/files/figures_5class.h5"
@@ -83,15 +83,15 @@ def file_download():
 
     print("EXPERIMENT #3 LOADED") 
 
-    captions_2clusters_cross = "captions_2clusters_cross.h5"
-    url = "https://zenodo.org/record/1442704/files/captions_2clusters_cross.h5"
-    r = requests.get(url, allow_redirects=True)
-    open(captions_2clusters_cross, 'wb').write(r.content)
+#    captions_2clusters_cross = "captions_2clusters_cross.h5"
+#    url = "https://zenodo.org/record/1442704/files/captions_2clusters_cross.h5"
+#    r = requests.get(url, allow_redirects=True)
+#    open(captions_2clusters_cross, 'wb').write(r.content)
     
-    captions_2clusters_cross_weights = "captions_2clusters_cross_weights.h5"
-    url = "https://zenodo.org/record/1442704/files/captions_2clusters_cross_weights.h5"
-    r = requests.get(url, allow_redirects=True)
-    open(captions_2clusters_cross_weights, 'wb').write(r.content)
+#    captions_2clusters_cross_weights = "captions_2clusters_cross_weights.h5"
+#    url = "https://zenodo.org/record/1442704/files/captions_2clusters_cross_weights.h5"
+#    r = requests.get(url, allow_redirects=True)
+#    open(captions_2clusters_cross_weights, 'wb').write(r.content)
 
     captions_5class_cross = "captions_5class_cross.h5"
     url = "https://zenodo.org/record/1442704/files/captions_5class_cross.h5"
@@ -104,15 +104,15 @@ def file_download():
     open(captions_5class_cross_weights, 'wb').write(r.content)
 	
 
-    figures_2clusters_cross = "figures_2clusters_cross.h5"
-    url = "https://zenodo.org/record/1442704/files/figures_2clusters_cross.h5"
-    r = requests.get(url, allow_redirects=True)
-    open(figures_2clusters_cross, 'wb').write(r.content)
+#    figures_2clusters_cross = "figures_2clusters_cross.h5"
+#    url = "https://zenodo.org/record/1442704/files/figures_2clusters_cross.h5"
+#    r = requests.get(url, allow_redirects=True)
+#    open(figures_2clusters_cross, 'wb').write(r.content)
     
-    figures_2clusters_cross_weights = "figures_2clusters_cross_weights.h5"
-    url = "https://zenodo.org/record/1442704/files/figures_2clusters_cross_weights.h5"
-    r = requests.get(url, allow_redirects=True)
-    open(figures_2clusters_cross_weights, 'wb').write(r.content)
+#    figures_2clusters_cross_weights = "figures_2clusters_cross_weights.h5"
+#    url = "https://zenodo.org/record/1442704/files/figures_2clusters_cross_weights.h5"
+#    r = requests.get(url, allow_redirects=True)
+#    open(figures_2clusters_cross_weights, 'wb').write(r.content)
 
     figures_5class_cross = "figures_5class_cross.h5"
     url = "https://zenodo.org/record/1442704/files/figures_5class_cross.h5"
@@ -126,30 +126,30 @@ def file_download():
     
     print("EXPERIMENT #4 LOADED")
 	
-    quality2clusters = "quality2clusters.h5"
-    url = "https://zenodo.org/record/1442704/files/quality2clusters.h5"
-    r = requests.get(url, allow_redirects=True)
-    open(quality2clusters, 'wb').write(r.content)
+#    quality2clusters = "quality2clusters.h5"
+#    url = "https://zenodo.org/record/1442704/files/quality2clusters.h5"
+#    r = requests.get(url, allow_redirects=True)
+#    open(quality2clusters, 'wb').write(r.content)
 	
     quality5class = "quality5class.h5"
     url = "https://zenodo.org/record/1442704/files/quality5class.h5"
     r = requests.get(url, allow_redirects=True)
     open(quality5class, 'wb').write(r.content)
     
-    qualityMix2clusters = "qualityMix2clusters.h5"
-    url = "https://zenodo.org/record/1442704/files/qualityMix2clusters.h5"
-    r = requests.get(url, allow_redirects=True)
-    open(qualityMix2clusters, 'wb').write(r.content)
+#    qualityMix2clusters = "qualityMix2clusters.h5"
+#    url = "https://zenodo.org/record/1442704/files/qualityMix2clusters.h5"
+#    r = requests.get(url, allow_redirects=True)
+#    open(qualityMix2clusters, 'wb').write(r.content)
 	
     qualityMix5class = "qualityMix5class.h5"
     url = "https://zenodo.org/record/1442704/files/qualityMix5class.h5"
     r = requests.get(url, allow_redirects=True)
     open(qualityMix5class, 'wb').write(r.content)
 	
-    qualityUni2clusters = "qualityUni2clusters.h5"
-    url = "https://zenodo.org/record/1442704/files/qualityUni2clusters.h5"
-    r = requests.get(url, allow_redirects=True)
-    open(qualityUni2clusters, 'wb').write(r.content)
+#    qualityUni2clusters = "qualityUni2clusters.h5"
+#    url = "https://zenodo.org/record/1442704/files/qualityUni2clusters.h5"
+#    r = requests.get(url, allow_redirects=True)
+#    open(qualityUni2clusters, 'wb').write(r.content)
 	
     qualityUni5class = "qualityUni5class.h5"
     url = "https://zenodo.org/record/1442704/files/qualityUni5class.h5"

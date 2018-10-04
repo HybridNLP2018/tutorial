1dfb9c25adb00717cf6d4ca73ee07276f7824a9e
Ronald Denaux <rdenaux@expertsystem.com>
2018-10-04 10:09:21 +0200

    Fixed bug in wntoken

--- scripts/wntoken.py
+++ scripts/wntoken.py
@@ -101,7 +101,7 @@ def open_as_token_dicts(file_name, token_format='tlgs',
                         include_glossa=True):
     '''Returns a list of token dictionaries'''
     toked_lines = []
-    with open('_tlgs_wnscd', encoding='utf8') as f:
+    with open(file_name, encoding='utf8') as f:
         lnum = 1
         for line in f.readlines():
             toks = [tok for tok in line.split()]
